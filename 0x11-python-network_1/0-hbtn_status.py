5a2278e62af3feaaeff3150a14b75fdec32dd014
uNyanda <ayabulelamdlalose@outlook.com>
2024-04-27 05:42:34 +0200

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode("utf-8")
+
+    print("Body response:")
+    print(f"    - type: {type(content)}")
+    print(f"    - content: {content}")
+    print(f"    - utf8 content: {utf8_content}")
