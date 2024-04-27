dfd076d5f27fc0582a3b2ee376fd870cb8301674
uNyanda <ayabulelamdlalose@outlook.com>
2024-04-27 05:58:11 +0200

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script sends a POST request to a given URL with an email as a parameter
+and displays the response body.
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # get url from commandline args
+    email = sys.argv[2]  # get the email from commandline args
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
+    # create a request and send it
+    with urllib.request.urlopen(url, data=data) as response:
+        content = response.read().decode("utf-8")
+
+    print(f"Your email is: {email}")
+    print(f"Response body: {content}")
