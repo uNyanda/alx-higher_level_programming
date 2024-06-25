39434ee690d949bcf874d44ff7de89ee236bd031
uNyanda <ayabulelamdlalose@outlook.com>
2024-06-25 15:14:47 +0200

    filter states.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This script filters out the names"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Check if correct number of arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Gather arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL
+    try:
+        db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                             passwd=password, db=database)
+        cursor = db.cursor()
+
+        # Execute the query
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER\
+                   BY id ASC")
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Print each ro in the required format
+        for row in rows:
+            print(row)
+
+        # Close the cursor and connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
