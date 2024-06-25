40dc42a52d6ad79016912986da0b54962efba4cc
uNyanda <ayabulelamdlalose@outlook.com>
2024-06-25 10:40:26 +0200

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
 """Module to list all states from a database"""
+import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+    # Check if correct number of arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Gather arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password,
+                             database=database)
+        cursor = db.cursor()
+
+        # Execute the query
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Print each row in the required format
+        for row in rows:
+            print(row)
+
+        # Close cursor and connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
