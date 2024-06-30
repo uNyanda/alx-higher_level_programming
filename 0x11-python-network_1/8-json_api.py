13560ab7163212f24916dd1481f065514a485d19
uNyanda <ayabulelamdlalose@outlook.com>
2024-06-30 11:39:58 +0200

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,15 +7,20 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
+def fetch_user():
+    """
+    Handles sending a POST request with a letter as a parameter
+    from the command line and process the JSON response.
+    """
     # Get letter from command-line arguments or set to empty string if no
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
 
     # Prepare the data for the POST request
+    url = 'http://0.0.0.0:5000/search_user'
     data = {'q': letter}
 
     # Send the POST request
-    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    response = requests.post(url, data=data)
 
     try:
         # Attempt to parse the JSON respons
@@ -28,3 +33,7 @@ if __name__ == "__main__":
             print('No result')
     except ValueError:
         print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    fetch_user()
