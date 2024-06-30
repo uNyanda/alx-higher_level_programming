2482c7b724c57d573031ca9e0e5ad104f55e2d0b
uNyanda <ayabulelamdlalose@outlook.com>
2024-06-30 12:06:59 +0200

    github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script takes GitHub username and personal access token as arguments
+and uses the GitHub API to display the user's ID.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: {} <username> <token>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = f'https://api.github.con/users/{username}'
+    headers = {
+        "Authorization": f'token {token}',
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get('id'))
+        else:
+            print(None)
+    except requests.RequestException as e:
+        print(f'Request failed: {e}')
+        sys.exit(1)
