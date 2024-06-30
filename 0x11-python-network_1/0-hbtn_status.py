342eda9e28940547a9c41b4e223e50208883cf86
uNyanda <ayabulelamdlalose@outlook.com>
2024-06-30 10:06:07 +0200

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """
 This script fetches the status from https://alx-intranet.hbtn.io/status
+using the urllib package and displays the body of the response.
 """
 import urllib.request
 
 
-def main():
+def fetch_status():
+    """
+    Fetches the status from the link
+    and displays the body of the response.
+    """
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode("utf-8")
 
     print("Body response:")
-    print(f"    - type: {type(content)}")
-    print(f"    - content: {content}")
-    print(f"    - utf8 content: {utf8_content}")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {utf8_content}")
 
 
 if __name__ == "__main__":
-    main()
+    fetch_status()
